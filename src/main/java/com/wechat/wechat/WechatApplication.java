104dbabac1594164a1d388d0b71a0ddf609baf0e
wanyang <605968060@qq.com>
2018-07-04 11:58:21 +0800

    fix:项目启动

--- src/main/java/com/wechat/wechat/WechatApplication.java
+++ src/main/java/com/wechat/wechat/WechatApplication.java
@@ -2,15 +2,9 @@ package com.wechat.wechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SuppressWarnings("PMD")
-@EnableDiscoveryClient
-@EnableCircuitBreaker
-@EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class WechatApplication {
 
 	public static void main(String[] args) {
